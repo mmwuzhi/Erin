992191b63043816f6d477b966f144eacdc96098d
mmwuzhi <55689068+mmwuzhi@users.noreply.github.com>
2023-08-12 15:12:10 +0900

    feat: make FullscreenFilterContainer more generic
    customizable z-index and enableContextMenu
    optional for 'children' props

--- components/FullscreenFilterContainer.tsx
+++ components/FullscreenFilterContainer.tsx
@@ -1,25 +1,32 @@
-import { EZIndexRecord } from "~types"
 import clsx from "clsx"
+
+import { EZIndexRecord } from "~types"
 import { onStopPaClickPropagation } from "~utils/browser"
 
 export default function ({
-  children,
+  children = null,
   onClickOutside,
-  classnames
+  classnames,
+  enableContextMenu = false,
+  zIndex = EZIndexRecord.fullscreenLayout
 }: {
-  children: React.ReactNode
+  children?: React.ReactNode
   onClickOutside?: () => void
   classnames?: string
+  enableContextMenu?: boolean
+  zIndex?: number
 }) {
   return (
     <div
       className={clsx("fixed inset-0 w-screen h-screen", classnames)}
-      style={{zIndex: EZIndexRecord.fullscreenLayout}}
+      style={{ zIndex }}
       onClick={() => {
         onClickOutside?.()
       }}
-      onContextMenu={onStopPaClickPropagation}>
-      <div onClick={onStopPaClickPropagation} className="inline-block">
+      onContextMenu={enableContextMenu ? undefined : onStopPaClickPropagation}>
+      <div
+        onClick={enableContextMenu ? undefined : onStopPaClickPropagation}
+        className="inline-block">
         {children}
       </div>
     </div>
